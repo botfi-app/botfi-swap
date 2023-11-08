266ebef0e048d761ce37ab31293c9d6ae128a772
BigZak <73213378+big-zak@users.noreply.github.com>
2023-11-08 04:31:34 +0000

    Update SwapEngine.sol

--- contracts/core/SwapEngine.sol
+++ contracts/core/SwapEngine.sol
@@ -42,45 +42,54 @@ contract SwapEngine is ContractBase {
         );
 
         if(isNew) dexes.push(dex);
-    
     }
 
     /**
     * @dev get the swap amount out for uni v2 
     * @param amountIn the amount to be swapped 
-    * @param pairToken the pair token to swap
+    * @param path the array path of tokenA and B
     * @param router the swap router
     * @param slippageBps the slipage tolerance in basis point
     */
-    function __getV2AmountOut(
+    function __getV2AmountOutMin(
         uint256 amountIn, 
-        address pairToken,
+        address[] memory path, 
         IUniswapV2Router02 router, 
         uint256 slippageBps
     )
         internal 
+        view
         returns(uint256)
     {
+        uint256[] memory amounts = router.getAmountsOut(amountIn, path);
 
-        uint slipageAm
-        IUniswapV2Pair pairToken = IUniswapV2Pair(pairToken);
+        uint256 amountOut = amounts[1];
 
-        return 
+        // lets get the slippage amount 
+        if(amountOut > 0 && slippageBps > 0){
+            uint slippageAmt = calPercentage(amountOut, slippageBps);
+            amountOut -= slippageAmt;
+        }
+
+        return amountOut;
     } 
 
     /**
      * @dev swap uniswap v2
+     * @param dex the name of the dex in bytes32
      * @param amount the input amount
-     * @param tokenIn the input token address 
-     * @param tokenOut the output token address
+     * @param tokenB the input token address 
+     * @param tokenB the output token address
+     * @param slippageBps the slippage in basis point
+     * @param supportsFeeOnTransfer wether the token supports fee on transfer
      */
     function swapV2(
         bytes32 dex,
         uint256 amount, 
         address tokenA, 
         address tokenB,
-        uint    slippage,
-        bool    tokenSupportingFee
+        uint    slippageBps,
+        bool    supportsFeeOnTransfer
     ) 
         public 
         payable
@@ -88,16 +97,16 @@ contract SwapEngine is ContractBase {
 
         require(tokenA != tokenB, "BotFi: TOKEN_A_B_SAME");
         require(amount > 0, "BotFi: ZERO_AMOUNT");
-        require(slippage > 0, "BotFi: ZERO_SLIPPAGE");
+        require(slippageBps > 0, "BotFi: ZERO_SLIPPAGE");
 
         DexParams memory _dexInfo = dexesParams[dex];
 
         require(_dexInfo.createdAt > 0, "BotFi: UNSUPPORTED_DEX");
         require(address(_dexInfo.v2Router) != address(0), "BotFi: V2_SWAP_UNSUPPORTED");
 
-        uint feeAmt;
         address weth = _dexInfo.v2Router.WETH();
         address recipient = address(this);
+        uint deadline = block.timestamp + 60; // 1min
 
         if(tokenA == nativeToken) {
             
@@ -118,14 +127,41 @@ contract SwapEngine is ContractBase {
             tokenB = weth;
         }
 
-        address[2] memory pair = [tokenA, tokenB];
+        address[] memory path = new address[](2);
+        path[0] = tokenA;
+        path[1] = tokenB;
 
         uint feeAmt = calPercentage(amount, protocolFee);
         
-        uint256 amountWithFee = amount - feeAmt;
+        uint256 amountIn = amount - feeAmt;
 
-        uint256 slippageAmt = calPercentage(amountWithFee, slippage);
+        uint256 amountOutMin = __getV2AmountOutMin(
+                                    amountIn, 
+                                    path,
+                                    _dexInfo.v2Router, 
+                                    slippageBps
+                                );
 
+        if(tokenA == weth){
+
+            if(supportsFeeOnTransfer){
+                _dexInfo.v2Router
+                    .swapExactETHForTokensSupportingFeeOnTransferTokens{ value: amountIn }(
+                        amountOutMin,
+                        path,
+                        recipient, 
+                        deadline
+                    );
+            } else {
+                _dexInfo.v2Router
+                        .swapETHForExactTokens{ value: amountIn }(
+                            amountOutMin,
+                            path,
+                            recipient,
+                            deadline
+                        );
+                }
+        }
      
     } //end swap v2
 
