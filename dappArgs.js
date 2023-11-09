bc1c2a9a43946b60215b675d45c426c5223e4973
BigZak <73213378+big-zak@users.noreply.github.com>
2023-11-09 10:55:46 +0000

    update

--- dappArgs.js
+++ dappArgs.js
@@ -3,6 +3,6 @@ module.exports = {
     // protocol fee percent
     protocolFeeBasisPoint: 100, //1% * 100 = 100
     
-    // the protocol fee wallet 
-    feeWallet: "0x92c9E17a2f02b2119350F3E90918bEd3c6e24F3c"
-}
+    // the protocol fee wallet only for dev
+    feeWallet: "0x92c9E17a2f02b2119350F3E90918bEd3c6e24F3c" 
+} 
