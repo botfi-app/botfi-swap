a9f319547c5d72048244aab214b1b8a8b54d39bd
Bot Papi <botfi.app@gmail.com>
2023-11-12 23:49:35 +0000

    multicall library changed from openzeppellin to markerDao's

--- contracts/ContractBase.sol
+++ contracts/ContractBase.sol
@@ -5,14 +5,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "./interfaces/@uniswap/v2/v2-periphery/interfaces/IWETH.sol";
-import "@openzeppelin/contracts/utils/Multicall.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./interfaces/@uniswap/v2/v2-periphery/interfaces/IUniswapV2Router02.sol";
-//import "./interfaces/@uniswap/v2/v2-core/interfaces/IUniswapV2Factory.sol";
-//import "./interfaces/@uniswap/v3/v3-core/interfaces/IUniswapV3Factory.sol";
-//import "./interfaces/@uniswap/v3/v3-periphery/interfaces/ISwapRouter.sol";
-//import "./interfaces/@uniswap/v2/v2-core/interfaces/IUniswapV2Pair.sol";
+import "./multicall/Multicall3.sol";
 
 import "./base/Utils.sol";
 import "./base/Defs.sol";
@@ -21,7 +17,7 @@ contract ContractBase is
     Defs, 
     Context, 
     Ownable, 
-    Multicall, 
+    Multicall3, 
     ReentrancyGuard 
 {
 
