70c5fa3821d3073f08336cbb342fceeeae081dc6
Bot Papi <botfi.app@gmail.com>
2023-11-27 01:36:53 +0000

    update

--- dappArgs.js
+++ dappArgs.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-    // protocol fee percent
-    protocolFeeBasisPoint: 8000, //0.8%, 0.1 = 1000bps as its divided by 10_000
+    // protocol fee percent percentage * 100 = basis point
+    protocolFeeBasisPoint: 80, //0.8% = 80, 0.1 = 10bps as its divided by 10_000
     
     // the protocol fee wallet only for dev
     feeWallet: "0x92c9E17a2f02b2119350F3E90918bEd3c6e24F3c" 
